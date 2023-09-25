a0c7287fbf5bcaa7a670b15d2f0090136c4ab793
Kaycee <139760804+kayceed123@users.noreply.github.com>
2023-09-25 10:27:18 +0100

    Add files via upload

---
+++ Python/Lesson1 challenges.py
@@ -0,0 +1,59 @@
+#First name
+#firstname=input("What is your name?")
+#print("your firstname is", firstname)
+
+#Surname
+#surname=input("What is your surname?")
+#print("your surname is", surname)
+#print("Hello", firstname, surname)
+
+#initials
+#print("Your initials are", firstname[0],surname[0])
+
+#Full name
+#fullname=firstname + " " + surname
+#print("Your full name is", fullname)
+
+#Capitals
+#capitalsurname=surname.upper()
+#print(capitalsurname + " " + firstname)
+
+#Length of surname
+#surnamelength= str(len(surname))
+#print(surnamelength)
+
+#username
+#threeletters= surname[0:3]
+#firstinitial= firstname[0]
+#username= threeletters + firstinitial + surnamelength
+#print(username)
+
+#sleep calculator
+#hourspernight= int(input("How many hours do you sleep on a night?"))
+#print("You sleep", hourspernight, "hours per night")
+#hoursperweek= hourspernight * 7
+#print("You sleep", hoursperweek, "hours per week")
+#hourspermonth= float(hoursperweek)* 4.35
+#print("You sleep", hourspermonth, "hours per month")
+#dayspermonth=float(hourspermonth)/24
+#print("This equates to", dayspermonth, "days per month")
+
+#Extension- app bill
+#if firstname== "Kaycee" and surname=="Duffey":
+    #print("welcome")
+    #network=input("Input your mobile phone network")
+    #minutes=int(input("how many minutes have you used this month?"))
+    #minprice= minutes * 0.10
+    #numoftexts=int(input("How many messages have you sent this month?"))
+    #textsprice= numoftexts * 0.05
+    #bill= minprice + textsprice
+    #print("Your bill for this month is £", bill)
+#else:
+    #print("not welcome")
+
+#Reverse words
+word=input("Enter a word")
+wordlength= len(word)
+for x in range (wordlength):
+    print(wordlength)
+
